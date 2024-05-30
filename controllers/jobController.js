5cebb259dc711519ef74990f5d40ef22d408612a
hassan1032 <khanhassan1032@gmail.com>
2024-05-30 11:57:18 +0530

    Changes Controller files

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -22,56 +22,48 @@ export const postJob = catchAsyncError(async (req, res, next) => {
   }
   const {
     title,
-    company,
     description,
-    catagerory,
+    category,
     country,
     city,
     location,
-    fixedsalary,
-    salaryfrom,
+    fixedSalary,
+    salaryFrom,
     salaryTo,
+    company
   } = req.body;
 
-  if (
-    !title ||
-    !description ||
-    !catagerory ||
-    !country ||
-    !city ||
-    !location ||
-    !company
-  ) {
+  if (!title || !description || !category || !country || !city || !location) {
     return next(new ErrorHandler("Please provide full job details.", 400));
   }
 
-  // if ((!salaryfrom || !salaryTo) && !fixedsalary) {
-  //   return next(
-  //     new ErrorHandler(
-  //       "Please either provide fixed salary or ranged salary.",
-  //       400
-  //     )
-  //   );
-  // }
+  if ((!salaryFrom || !salaryTo) && !fixedSalary) {
+    return next(
+      new ErrorHandler(
+        "Please either provide fixed salary or ranged salary.",
+        400
+      )
+    );
+  }
 
-  if (salaryfrom && salaryTo && fixedsalary) {
+  if (salaryFrom && salaryTo && fixedSalary) {
     return next(
       new ErrorHandler("Cannot Enter Fixed and Ranged Salary together.", 400)
     );
   }
-  const jobPostedby = req.user._id;
+  const postedBy = req.user._id;
   const jobs = await job.create({
     title,
-    company,
     description,
-    catagerory,
+    category,
     country,
     city,
     location,
-    fixedsalary,
-    salaryfrom,
+    fixedSalary,
+    salaryFrom,
     salaryTo,
-    jobPostedby,
+    postedBy,
+    company
   });
   res.status(200).json({
     success: true,
@@ -79,7 +71,6 @@ export const postJob = catchAsyncError(async (req, res, next) => {
     jobs,
   });
 });
-
 // Get Job For Empolyer
 export const getmyJobs = catchAsyncError(async (req, res, next) => {
   const { role } = req.user;
--- models/jobSchema.js
+++ models/jobSchema.js
@@ -1,72 +1,69 @@
 import mongoose from "mongoose";
-import validator from "validator";
 
 const jobSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: [true, "Please Provide job title"],
-    minlength: [4, "job title must Contain at Least 4 characters!"],
-    maxlength: [50, " Job Title canot exceed 50 characters!"],
+    required: [true, "Please provide a title."],
+    minLength: [3, "Title must contain at least 3 Characters!"],
+    maxLength: [30, "Title cannot exceed 30 Characters!"],
   },
   description: {
     type: String,
-    required: [true, "Please Provide job description"],
-    minlength: [10, "job description must Contain at Least 10 characters!"],
-    maxlength: [300, " Job Description canot exceed 300 characters!"],
+    required: [true, "Please provide decription."],
+    minLength: [30, "Description must contain at least 30 Characters!"],
+    maxLength: [500, "Description cannot exceed 500 Characters!"],
   },
-  catagerory: {
+  category: {
     type: String,
-    required: [true, "Please Provide job catagerory"],
-    minlength: [4, "job catagerory must Contain at Least 4 characters!"],
-    maxlength: [50, " Job catagerory canot exceed 50 characters!"],
+    required: [true, "Please provide a category."],
   },
   country: {
     type: String,
-    required: [true, "Please Provide job country"],
+    required: [true, "Please provide a country name."],
   },
   city: {
     type: String,
-    required: [true, "Please Provide job city"],
+    required: [true, "Please provide a city name."],
   },
   location: {
     type: String,
-    required: [true, "Please Provide Exact  location"],
-    minlength: [true, "job location Must conatin at least 25 charcters!"],
+    required: [true, "Please provide location."],
+    minLength: [20, "Location must contian at least 20 characters!"],
   },
-  fixedsalary: {
+  fixedSalary: {
     type: Number,
-    // required: [true, "Please Provide job salary"],
-    min: [5, " Fixed Salary must be greater than 5!"],
-    max: [1000000, " Fixed Salary must be less than 1000000!"],
+    minLength: [4, "Salary must contain at least 4 digits"],
+    maxLength: [9, "Salary cannot exceed 9 digits"],
   },
-  salaryfrom: {
+  salaryFrom: {
     type: Number,
-    // required: [true, "Please Provide job salary from"],
-    min: [5, " Salary From must be greater than 5!"],
-    max: [1000000, " Salary From must be less than 1000000!"],
+    minLength: [4, "Salary must contain at least 4 digits"],
+    maxLength: [9, "Salary cannot exceed 9 digits"],
   },
-  salaryto: {
+  salaryTo: {
     type: Number,
-    min: [5, "Salary to must Contain at least 5 digits!"],
-    max: [100000, "Salary can not Exceed  100000 digits!"],
+    minLength: [4, "Salary must contain at least 4 digits"],
+    maxLength: [9, "Salary cannot exceed 9 digits"],
+  },
+  company: {
+    type: String,
+    required: [true, "Please provide company."],
+    minLength: [4, "Salary must contain at least 4 digits"],
+    maxLength: [9, "Salary cannot exceed 9 digits"],
   },
   expired: {
     type: Boolean,
     default: false,
   },
-  company: {
-    type: String,
-    required: [true, "Please Provide Company Name"],
-  },
-  jobPostedon: {
+  jobPostedOn: {
     type: Date,
     default: Date.now,
   },
-  jobPostedby: {
+  postedBy: {
     type: mongoose.Schema.ObjectId,
     ref: "User",
-    required: [true, "Please Provide job Posted by"],
+    required: true,
   },
 });
 
-export const job = mongoose.model("job", jobSchema);
+export const job = mongoose.model("Job", jobSchema);
