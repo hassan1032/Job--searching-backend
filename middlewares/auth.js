778f099d260ec0082b513bf0532b27589d478a27
hassan1032 <khanhassan1032@gmail.com>
2024-06-02 21:03:16 +0530

    changes code

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ import { User } from "../models/user.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthorized = catchAsyncError(async (req, res, next) => {
-  const { token } = req.cookies;
+  const token = req?.cookies?.token || req?.headers['authorization']?.split(" ")[1];
 
   if (!token) {
     return next(new ErrorHandler("User are not authorized", 401));
