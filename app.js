f160a0db01b6e6180557cad4be6b05eb3e0bd946
hassan1032 <khanhassan1032@gmail.com>
2024-06-03 13:23:46 +0530

    Add Code

--- app.js
+++ app.js
@@ -1,44 +1,43 @@
-import express from "express";
-// import dotenv from "dotenv";
-import { config } from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import fileUpload from "express-fileupload";
-import applicationRouter from "./routes/applicationRouter.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs/promises';
+import path from 'path';
+import express from 'express';
+import fileUpload from 'express-fileupload';
+import applicationRouter from './routes/applicationRouter.js';
+import jobRouter from './routes/jobRouter.js';
+import userRouter from './routes/userRouter.js';
+import { errorMiddleware } from './middlewares/error.js';
 
-import jobRouter from "./routes/jobRouter.js";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-import userRouter from "./routes/userRouter.js";
-import {errorMiddleware} from './middlewares/error.js'
+const tempDir = path.join(__dirname, 'temp');
+
+async function createTempDir() {
+  try {
+    await fs.mkdir(tempDir);
+    console.log(`Directory '${tempDir}' created successfully.`);
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      console.log(`Directory '${tempDir}' already exists.`);
+    } else {
+      console.error(`Error creating directory '${tempDir}': ${err}`);
+    }
+  }
+}
+
+createTempDir(); // Call the function to create the directory
 
 const app = express();
-// dotenv.config({path: './config/config.env'})
-config({ path: "./config/config.env" });
-const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    process.env.FRONTEND_URL
-  ],
-  methods: ["GET", "POST", "DELETE", "PUT"],
-  credentials: true
-};
 
-app.use(cors(corsOptions));
-app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({ useTempFiles: true, tempFileDir: tempDir }));
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/application', applicationRouter);
+app.use('/api/v1/job', jobRouter);
 
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/temp/",
-  })
-);
-app.use('/api/v1/user',userRouter);
-app.use('/api/v1/application',applicationRouter)
-app.use('/api/v1/job',jobRouter)
-
-
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 export default app;
